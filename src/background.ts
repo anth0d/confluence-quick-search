6c2173d5bbb72c1c0726c7e66117133781afe333
Anthony Davis <anthony@56k.dev>
2021-09-16 10:43:15 -0500

    chore: improve error logging

--- src/background.ts
+++ src/background.ts
@@ -4,22 +4,6 @@ import { getSiteUrl } from "./utils/data";
 
 const installHandler = (details: chrome.runtime.InstalledDetails): void => {
   _gaq.push(["_trackEvent", "install"]);
-  chrome.omnibox.onInputEntered.addListener(function (text) {
-    getSiteUrl()
-      .then((siteUrl) => {
-        if (!siteUrl) {
-          _gaq.push(["_trackEvent", "omnibox", "alert"]);
-          alert("Click the extension and set a Confluence URL");
-        } else {
-          _gaq.push(["_trackEvent", "omnibox", "success"]);
-          chrome.tabs.create({ url: `${siteUrl}/dosearchsite.action?queryString=${encodeURIComponent(text)}` });
-        }
-      })
-      .catch((err) => {
-        console.error(err);
-        _gaq.push(["_trackEvent", "omnibox", "error"]);
-      });
-  });
   if (details.reason !== "install") {
     // no need to show notification
     return;
@@ -48,6 +32,35 @@ const installHandler = (details: chrome.runtime.InstalledDetails): void => {
   );
 };
 
+const searchHandler = (text) => {
+  _gaq.push(["_trackEvent", "omnibox"]);
+  if (chrome.runtime.lastError) {
+    console.error(chrome.runtime.lastError.message);
+  }
+  getSiteUrl()
+    .then((siteUrl) => {
+      if (!siteUrl) {
+        _gaq.push(["_trackEvent", "omnibox", "alert"]);
+        alert("Click the extension and set a Confluence URL");
+      } else {
+        _gaq.push(["_trackEvent", "omnibox", "success"]);
+        chrome.tabs.create({ url: `${siteUrl}/dosearchsite.action?queryString=${encodeURIComponent(text)}` }, () => {
+          if (chrome.runtime.lastError) {
+            console.error(chrome.runtime.lastError.message);
+          }
+        });
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      _gaq.push(["_trackEvent", "omnibox", "error"]);
+    });
+};
+
 if (!chrome.runtime.onInstalled.hasListener(installHandler)) {
   chrome.runtime.onInstalled.addListener(installHandler);
+}
+
+if (!chrome.omnibox.onInputEntered.hasListener(searchHandler)) {
+  chrome.omnibox.onInputEntered.addListener(searchHandler);
 }
